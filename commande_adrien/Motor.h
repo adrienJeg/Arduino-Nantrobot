daae27fa13a257a3f060b4554dddb7aa6e0c4725
adrienJeg <52580865+adrienJeg@users.noreply.github.com>
2020-01-25 18:34:46 +0100

    Add files via upload

---
+++ commande_adrien/Motor.h
@@ -0,0 +1,23 @@
+/*
+  Motor.h - Library for controlling a motor.
+  Created by Adrien Jégourel, January 25, 2020.
+*/
+#pragma once
+
+#include "Arduino.h"
+
+class Motor
+{
+  private:
+    int PIN_DIR;
+    int PIN_PWM;
+    int PWM;
+    signed int dirReference;
+  public:
+    Motor(int PIN_DIR, int PIN_PWM, signed int dirReference); // Constructor
+    void setMotor();
+
+    // Getters and Setters
+    int getPWM(){return PWM;};
+    void setPWM(int newPWM){PWM = newPWM;};
+};
