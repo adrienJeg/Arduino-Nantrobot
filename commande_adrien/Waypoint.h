9c9508ffbf464f2e23df35afe22a6cf56985d4ca
benjaminbcmp <benjaminbcmp@gmail.com>
2020-01-25 18:59:53 +0100

    Add signatures in Waypoint.h

--- commande_adrien/Waypoint.h
+++ commande_adrien/Waypoint.h
@@ -2,9 +2,23 @@
 
 #include "Arduino.h"
 #include "Pose.h"
+#include <Vector.h>
 
 class Waypoint
 {
+  private:
+    Pose currentPose;
+    int currentIndex;
+    Vector<Pose> listWaypoints;
+
   public:
     Waypoint();
+    Waypoint(Vector<Pose> listWaypoints);
+    // Waypoint(float[] listX, float[] listY);
+    void next();
+    void add(Pose p);
+
+    Pose getCurrentPose() {return this->currentPose;};
+    Vector<Pose> getListWaypoints() {return this->listWaypoints;};
+
 };
